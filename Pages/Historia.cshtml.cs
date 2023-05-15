52e0241f116a3b00d178b556c1e09dee7f407d14
alstefanowski <ciuniekk@gmail.com>
2023-05-15 18:25:33 +0200

    Dodaj pliki projektów.

---
+++ Pages/Historia.cshtml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Leap_Year.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+using Leap_Year.Areas.Identity.Pages.Account;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Leap_Year.Pages
+{
+    public class HistoriaModel : PageModel
+    {
+        private readonly Leap_Year.Data.ApplicationDbContext _context;
+        private readonly IConfiguration _configuration;
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+
+        public PaginatedList<LeapYear> leapYears { get; set; }
+        public HistoriaModel(Leap_Year.Data.ApplicationDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        [BindProperty]
+        public LeapYear LeapYear { get; set; } = default!;
+
+        public async Task OnGetAsync(string sortOrder, string currentFilter, string searchString, int? pageIndex)
+        {
+            CurrentSort = sortOrder;
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            CurrentFilter = searchString;
+            
+            IQueryable<LeapYear> _LeapYear = from s in _context.LeapYear select s;
+
+            /*
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    _LeapYear = _LeapYear.OrderByDescending(s => s.Name);
+                    break;
+                case "Date":
+                    _LeapYear = _LeapYear.OrderBy(s => s.SearchTime);
+                    break;
+                case "date_desc":
+                    _LeapYear = _LeapYear.OrderByDescending(s => s.SearchTime);
+                    break;
+                default:
+                    _LeapYear = _LeapYear.OrderBy(s => s.Year);
+                    break;
+            }
+            */
+            _LeapYear = _LeapYear.OrderByDescending(s => s.SearchTime);
+            var pageSize = _configuration.GetValue("PageSize", 20);
+            leapYears = await PaginatedList<LeapYear>.CreateAsync(
+                _LeapYear.AsNoTracking(), pageIndex ?? 1, pageSize);
+        }
+
+        /*
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.LeapYear == null)
+            {
+                return Page();
+            }
+            _context.LeapYear.Add(LeapYear);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+        */
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using Leap_Year.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Configuration;
+using System.Drawing.Printing;
+using Microsoft.AspNetCore.Identity;
+
+namespace Leap_Year.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly ApplicationDbContext _dbContext;
+
+        [BindProperty]
+        public LeapYear LeapYear { get; set; }
+
+        public IdentityUser user { get; set; }
+        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _dbContext = context;
+        }
+        [BindProperty]
+        public IList<LeapYear> Search_List { get; set; }
+     
+
+        [AllowAnonymous]
+        public IActionResult OnPost()
+        {
+            if ((LeapYear.Year % 400 == 0))
+            {
+                LeapYear.Result = "Rok przystepny";
+            }
+            else if (LeapYear.Year % 100 == 0)
+            {
+                LeapYear.Result = "Rok nie jest przystepny";
+            }
+
+            else if (LeapYear.Year % 4 == 0)
+            {
+                LeapYear.Result = "Rok przystepny";
+            }
+            else
+            {
+                LeapYear.Result = "Rok nie jest przystepny";
+            }
+           // if (LeapYear.Name == null)
+              //  LeapYear.Name = "";
+            LeapYear.SearchTime = DateTime.Now;
+            _dbContext.LeapYear.Add(LeapYear);
+            _dbContext.SaveChanges();
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            return RedirectToPage("./Index");
+        }
+    }
+}
